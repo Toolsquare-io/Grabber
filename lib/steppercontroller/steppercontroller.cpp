58ce319910002243119fe7c01509a064debf19bd
David Truyens <david@toolsquare.io>
2022-04-30 14:56:53 +0200

    adding stepper class

--- lib/steppercontroller/steppercontroller.cpp
+++ lib/steppercontroller/steppercontroller.cpp
@@ -10,14 +10,10 @@ steppercontroller::steppercontroller() {
 }
 
 void steppercontroller::setup() {
-    pinMode(thePins.M1_DIR_PIN, OUTPUT);
-    pinMode(thePins.M1_STP_PIN, OUTPUT);
-    pinMode(thePins.M1_DIR_PIN, OUTPUT);
-    pinMode(thePins.M2_STP_PIN, OUTPUT);
-    pinMode(thePins.M2_DIR_PIN, OUTPUT);
-    pinMode(thePins.Z_STP_PIN, OUTPUT);
-    pinMode(thePins.Z_DIR_PIN, OUTPUT);
-    pinMode(thePins.SHIELD_EN_PIN, OUTPUT);
+    M1.setPins(thePins.M1_STP_PIN, thePins.M1_DIR_PIN);
+    M2.setPins(thePins.M2_STP_PIN, thePins.M2_DIR_PIN);
+    MZ.setPins(thePins.Z_STP_PIN, thePins.Z_DIR_PIN);
+
     digitalWrite(thePins.SHIELD_EN_PIN, LOW);
 
     pinMode(thePins.x_min_limitpin, INPUT_PULLUP);
@@ -44,31 +40,35 @@ void steppercontroller::run(inputStates newPosition) {
                 break;
             case inputStates::Xminus:
                 if (xminlimitState) {
-                    coreStep();
+                    M1.step();
+                    M2.step();
                 }
                 break;
 
             case inputStates::Xplus:
                 if (xpluslimitState) {
-                    coreStep();
+                    M1.step();
+                    M2.step();
                 }
                 break;
 
             case inputStates::Yminus:
                 if (yminlimitState) {
-                    coreStep();
+                    M1.step();
+                    M2.step();
                 }
                 break;
 
             case inputStates::Yplus:
                 if (ypluslimitState) {
-                    coreStep();
+                    M1.step();
+                    M2.step();
                 }
                 break;
 
             case inputStates::Zminus:        // TODO add z limits
             case inputStates::Zplus:
-                zStep();
+                MZ.step();
                 break;
             default:
                 break;
@@ -94,7 +94,7 @@ void steppercontroller::checklimits() {
         xpluslimitState = new_xpluslimit;
         theLog.snprintf(subSystem::stepper, loggingLevel::Warning, "xplus limitstate %c ", xpluslimitState);
     }
-    
+
     if (new_yminlimit != yminlimitState) {
         yminlimitState = new_yminlimit;
         theLog.output(subSystem::stepper, loggingLevel::Warning, "ymin limitstate");
@@ -104,50 +104,35 @@ void steppercontroller::checklimits() {
         theLog.output(subSystem::stepper, loggingLevel::Warning, "yplus limitstate");
     }
 
-
     // TODO add z limits
-}
-
-void steppercontroller::coreStep() {
-    // theLog.output(subSystem::general,loggingLevel::Debug,"core step");
-    digitalWrite(thePins.M1_STP_PIN, HIGH);
-    digitalWrite(thePins.M2_STP_PIN, HIGH);
-    delay(1);
-    digitalWrite(thePins.M1_STP_PIN, LOW);
-    digitalWrite(thePins.M2_STP_PIN, LOW);
-}
-
-void steppercontroller::zStep() {
-    digitalWrite(thePins.Z_STP_PIN, HIGH);
-    digitalWrite(thePins.Z_STP_PIN, LOW);
 }
 
 void steppercontroller::changeState() {
     switch (thePosition) {
         case inputStates::Xminus:
-            digitalWrite(thePins.M1_DIR_PIN, HIGH);
-            digitalWrite(thePins.M2_DIR_PIN, LOW);
+            M1.setDir(direction::clockwise);
+            M2.setDir(direction::counterclockwise);
             break;
 
         case inputStates::Xplus:
-            digitalWrite(thePins.M1_DIR_PIN, LOW);
-            digitalWrite(thePins.M2_DIR_PIN, HIGH);
+            M1.setDir(direction::counterclockwise);
+            M2.setDir(direction::clockwise);
 
         case inputStates::Yminus:
-            digitalWrite(thePins.M1_DIR_PIN, HIGH);
-            digitalWrite(thePins.M2_DIR_PIN, HIGH);
+            M1.setDir(direction::clockwise);
+            M2.setDir(direction::clockwise);
             break;
 
         case inputStates::Yplus:
-            digitalWrite(thePins.M1_DIR_PIN, LOW);
-            digitalWrite(thePins.M2_DIR_PIN, LOW);
+            M1.setDir(direction::counterclockwise);
+            M2.setDir(direction::counterclockwise);
 
         case inputStates::Zminus:
-            digitalWrite(thePins.Z_DIR_PIN, HIGH);
+            MZ.setDir(direction::clockwise);
             break;
 
         case inputStates::Zplus:
-            digitalWrite(thePins.Z_DIR_PIN, LOW);
+            MZ.setDir(direction::counterclockwise);
             break;
 
         default:
