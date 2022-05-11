7683ab89c65eedfa637840fae350eee562198ea3
Domien <domien.goffin@gmail.com>
2022-05-11 16:04:58 +0200

    Adding zpins

--- lib/IO/pinmapping.h
+++ lib/IO/pinmapping.h
@@ -3,9 +3,8 @@
 #pragma once
 
 class pinmapping {
+    // ESP32 has 21 pins
 
-  //ESP32 has 21 pins
-  
   public:
     // inputs
     int JoyStickXPosPin = 14;
@@ -17,21 +16,22 @@ class pinmapping {
     int GrabButtonPin   = 20;
 
     // CNCShield defined pin's:
-    int M1_STP_PIN = 2;
-    int M2_STP_PIN = 3;
-    int Z_STP_PIN = 4;
-    int M1_DIR_PIN = 5;
-    int M2_DIR_PIN = 6;
-    int Z_DIR_PIN = 7;
+    int M1_STP_PIN    = 2;
+    int M2_STP_PIN    = 3;
+    int Z_STP_PIN     = 4;
+    int M1_DIR_PIN    = 5;
+    int M2_DIR_PIN    = 6;
+    int Z_DIR_PIN     = 7;
     int SHIELD_EN_PIN = 8;
 
-    int x_plus_limitpin = 9;
-    int x_min_limitpin = 10;
-    int y_plus_limitpin = 11;
-    int y_min_limitpin = 12;
+    int x_plus_limitpin = 22;
+    int x_min_limitpin  = 23;
+    int y_plus_limitpin = 24;
+    int y_min_limitpin  = 25;
+    int z_plus_limitpin = 26;
+    int z_min_limitpin  = 27;
     // TODO add zpins
 
-    //other
+    // other
     int led_datapin = 21;
-
 };
